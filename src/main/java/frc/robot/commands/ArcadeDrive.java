c8ecfd85cdfc7116111d220923e4f068234acc39
nathannino <yohanpro.jimdo.com@gmail.com>
2019-01-15 19:05:24 -0500

    Add files via upload
    We added a starter code

---
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+/**
+ * An example command.  You can replace me with your own command.
+ */
+public class ArcadeDrive extends Command {
+  public ArcadeDrive() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.m_DriveBaseSub);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.m_DriveBaseSub.stopRobot();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.m_DriveBaseSub.arcadeDrive(Robot.m_oi.returnPilote().getX(),Robot.m_oi.returnPilote().getY());
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.m_DriveBaseSub.stopRobot();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveBaseSub.java
@@ -0,0 +1,89 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.ArcadeDrive;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class DriveBaseSub extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  AHRS ahrs;
+
+  private SpeedControllerGroup m_FrontSControlGroup;
+  private SpeedControllerGroup m_BackSControlGroup;
+
+  private DifferentialDrive m_DiffDrive;
+
+  private SpeedController m_LeftFrontMotor = RobotMap.LeftFrontMotor;
+  private SpeedController m_LeftBackMotor = RobotMap.LeftBackMotor;
+  private SpeedController m_RightFrontMotor = RobotMap.RightFrontMotor;
+  private SpeedController m_RightBackMotor = RobotMap.RightBackMotor;
+
+  
+
+  public DriveBaseSub() {
+    try {
+      /* Communicate w/navX-MXP via the MXP SPI Bus.                                     */
+      /* Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB     */
+      /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
+      ahrs = new AHRS(SPI.Port.kMXP); 
+    } catch (RuntimeException ex ) {
+      DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
+    }
+
+    m_FrontSControlGroup = new SpeedControllerGroup(m_LeftFrontMotor, m_RightFrontMotor);
+    m_BackSControlGroup = new SpeedControllerGroup(m_LeftBackMotor, m_RightBackMotor);
+
+    m_DiffDrive = new DifferentialDrive(m_FrontSControlGroup, m_FrontSControlGroup);
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+     setDefaultCommand(new ArcadeDrive());
+  }
+
+  public void stopRobot(){
+    m_FrontSControlGroup.set(0);
+    m_BackSControlGroup.set(0);
+  }
+
+
+
+  public void arcadeDrive(double xSpeed, double zRotation) {
+    m_DiffDrive.arcadeDrive(xSpeed, zRotation);
+  }
+
+  public void arcadeDrive(double xSpeed, double zRotation , boolean squareInputs) {
+    m_DiffDrive.arcadeDrive(xSpeed, zRotation, squareInputs);
+  }
+
+  public void curvatureDrive(double xSpeed, double zRotation, boolean isQuickTurn) {
+    m_DiffDrive.arcadeDrive(xSpeed, zRotation, isQuickTurn);
+
+  }
+
+  
+  
+}
