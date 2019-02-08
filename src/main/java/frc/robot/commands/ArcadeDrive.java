8a6798e5e7b533caf82cfb2e8d4b3b022c368773
chien <jeanphilippe.blouin@gmail.com>
2019-02-07 19:10:34 -0500

    Using new TuningPilotAxis from OI

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -16,8 +16,6 @@ import frc.robot.Robot;
  * An example command.  You can replace me with your own command.
  */
 public class ArcadeDrive extends Command {
-  //TuningPilotAxis m_forwardtuning;
-  //TuningPilotAxis m_rotatetuning;
 
   public ArcadeDrive() {
     // Use requires() here to declare subsystem dependencies
@@ -33,8 +31,8 @@ public class ArcadeDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    //double forward  = m_forwardtuning.getEnhancedAxis(Robot.m_oi.returnPilote().getX());
-    //double rotation = m_rotatedtuning.getEnhancedAxis(Robot.m_oi.returnPilote().getY());
+    //double forward  = Robot.m_oi.getPilotForwardTuning().getEnhancedAxis(Robot.m_oi.returnPilote().getX());
+    //double rotation = Robot.m_oi.getPilotRotationTuning().getEnhancedAxis(Robot.m_oi.returnPilote().getY());
     //Robot.m_DriveBaseSub.arcadeDrive(forward, rotation);
 
     Robot.m_DriveBaseSub.arcadeDrive(Robot.m_oi.returnPilote().getX(),Robot.m_oi.returnPilote().getY());
